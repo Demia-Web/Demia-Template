24f309b845a1a538fb081de78d10d05786a81bf9
Niko <nicolo.bassi@demia.org>
2025-09-12 16:54:05 +0200

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://demia.agency",
   vite: {
     plugins: [tailwindcss()],
   },
