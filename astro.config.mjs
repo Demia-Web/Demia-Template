569e7872eac8611c285701f4b23157c3532feec1
Niko <nicolobassi58@gmail.com>
2025-10-08 09:42:17 +0200

    feat: integrate React and related dependencies; update configuration and styles for enhanced UI components

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import netlify from "@astrojs/netlify";
 
 import sitemap from "@astrojs/sitemap";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -19,5 +21,5 @@ export default defineConfig({
 
   adapter: netlify(),
   output: "server",
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
 });
